7b06546084bcda7f54780821c10d9122d4ddd902
Radfer10 <155971968+Radfer10@users.noreply.github.com>
2024-02-19 23:23:16 +0100

    Add files via upload

---
+++ libft2/ft_strchr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-migu <rde-migu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 15:09:09 by shzhu             #+#    #+#             */
+/*   Updated: 2024/02/19 21:29:51 by rde-migu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	unsigned char	b;
+	int				i;
+
+	b = (unsigned char)c;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((unsigned char)str[i] == b)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (b == '\0')
+		return ((char *)&str[i]);
+	return (NULL);
+}
+/*int main()
+{
+	const char	*str = "Hola que tal";
+	char		c = 'H';
+	char		result = *ft_strchr(str, c);
+	printf("%c\n", result);
+	return (0);
+}*/
