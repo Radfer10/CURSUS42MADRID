8a7e0ef3d04e3abe2dcbe7d520d4fa12c575ce4b
Radfer10 <155971968+Radfer10@users.noreply.github.com>
2024-02-20 21:08:41 +0100

    Add files via upload

---
+++ libft3/ft_strchr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-migu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 20:14:21 by rde-migu          #+#    #+#             */
+/*   Updated: 2024/02/20 20:14:30 by rde-migu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	unsigned char	b;
+	int				i;
+
+	b = (unsigned char)c;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((unsigned char)str[i] == b)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (b == '\0')
+		return ((char *)&str[i]);
+	return (NULL);
+}
+/*int main()
+{
+	const char	*str = "Hola que tal";
+	char		c = 'H';
+	char		result = *ft_strchr(str, c);
+	printf("%c\n", result);
+	return (0);
+}*/
