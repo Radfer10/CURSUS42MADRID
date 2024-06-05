aec2983f5832e6bf53d281f945d55816fc24cfdd
Radfer10 <rauldemi2000@gmail.com>
2024-06-05 16:43:52 +0200

    16:45/05/06/2024

---
+++ Push_Swap 2/push_swap.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-migu <rde-migu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 20:06:52 by rde-migu          #+#    #+#             */
+/*   Updated: 2024/05/28 23:25:10 by rde-migu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int is_sorted(t_stack *stack) {
+    while (stack && stack->next) {
+        if (stack->content > stack->next->content) {
+            return 0;
+        }
+        stack = stack->next;
+    }
+    return 1;
+}
+
+void push_swap_main(t_push_swap *push_swap) {
+    if (is_sorted(push_swap->a))
+        return;
+
+    int size = stack_size(push_swap->a);
+
+    if (size == 2)
+        sort_2(push_swap);
+    else if (size == 3)
+        sort_3(push_swap);
+    else if (size == 4)
+        sort_4(push_swap);
+    else if (size == 5)
+        sort_5(push_swap);
+    else
+        k_sort(push_swap);
+}
---
+++ Push_Swap 2/sort4.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort4.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-migu <rde-migu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 20:05:52 by rde-migu          #+#    #+#             */
+/*   Updated: 2024/05/28 20:06:07 by rde-migu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void sort_4(t_push_swap *push_swap) {
+    if (is_sorted(push_swap->a))
+        return;
+
+    t_stack *min_node = find_min(push_swap->a);
+
+    while (push_swap->a != min_node) {
+        ra(push_swap);
+        printf("ra\n");
+    }
+
+    pb(push_swap);
+    printf("pb\n");
+
+    sort_3(push_swap);
+
+    pa(push_swap);
+    printf("pa\n");
+}
