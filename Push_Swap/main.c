d14f2311c592f2a61cc03ba202ad3a10b1c1fa17
Radfer10 <155971968+Radfer10@users.noreply.github.com>
2024-06-12 19:38:56 +0200

    19:39/12/06/2024

--- Push_Swap/main.c
+++ Push_Swap/main.c
@@ -23,10 +23,8 @@ void free_stack(t_stack **stack) {
     }
 }
 
-
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        /*printf("Usage: %s [numbers...]\n", argv[0]);*/
         return 1;
     }
 
@@ -35,33 +33,16 @@ int main(int argc, char *argv[]) {
     push_swap.b = NULL;
     push_swap.a_size = 0;
 
-    // Llamar a arg_parse para analizar los argumentos y llenar la pila
     arg_parse(&push_swap, argc, argv);
-
-    /*printf("Original stack: ");
-    print_stack(push_swap.a);*/
-
-    /*Aquí podrías añadir las funciones para asignar índices y ordenar si es necesario*/
     assign_indices(&(push_swap.a));
-    /*insertion_sort(&(push_swap.a));*/
-
-    /*printf("Stack with indices: ");*/
     t_stack *tmp = push_swap.a;
     while (tmp) {
-        /*printf("%d(%d) ", tmp->content, tmp->index);*/
         tmp = tmp->next;
     }
-    /*printf("\n");*/
-
-    // Llamada a push_swap
     push_swap_main(&push_swap);
-
-    /*printf("Sorted stack: ");
-    print_stack(push_swap.a);*/
-    
     free_stack(&push_swap.a);
     free_stack(&push_swap.b);
-    
-
     return 0;
 }
+
+
